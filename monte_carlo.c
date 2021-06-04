ddc0953a3c5e5c3a2eb53e4441758e25aa12b0dd
Swami-Malireddy <84972356+Swami-Malireddy@users.noreply.github.com>
2021-06-04 15:28:40 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,34 +38,16 @@ int main(void) {
     }
   }
 }
-float mc_pi(){
-  int interval, i;
-    double rand_x, rand_y, origin_dist, pi;
-    int circle_points = 0, square_points = 0;
-  
-   
-    srand(time(NULL));
-  
-    
-    for (i = 0; i < (INTERVAL * INTERVAL); i++) {
-  
-       
-        rand_x = double(rand() % (INTERVAL + 1)) / INTERVAL;
-        rand_y = double(rand() % (INTERVAL + 1)) / INTERVAL;
-  
-  
-        origin_dist = rand_x * rand_x + rand_y * rand_y;
-  
-        
-        if (origin_dist <= 1)
-            circle_points++;
-  
-        
-        square_points++;
-  
-       
-        pi = double(4 * circle_points) / square_points;
-
-
-return pi;
+float mc_pi(int a){
+    float count=0;
+    for(int i=0;i<a;i++){
+        float x=frandom();
+        float y=frandom();
+        float d=sqrt(pow(x,2)+pow(y,2));
+        if(d<1){
+            count++;
+        }
     }
+    float k = 4*count/a;
+    return k;
+}
